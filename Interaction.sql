62766eba0e0e26655fb2076eeb8bd7a332586de5
Lichao Duan <dlc9624@gmail.com>
2020-12-20 10:14:16 -0600

    Delete Interaction.sql

--- Interaction.sql
+++
@@ -1,8 +0,0 @@
-use ClientInformationSystem
-
-SET IDENTITY_INSERT Interactions ON;
-
-insert into Interactions(id,ClientId,EmployeeId,IntType,IntDate,Remarks) values(1,1,1,'S', '2019-12-18T15:19:06.7133333', 'First Interview');
-
-
-SET IDENTITY_INSERT Interactions OFF;
